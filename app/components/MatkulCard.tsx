348324a4ff9fe95dcc4ba803df1d7d73a8e7805d
tipen7 <tipdyanizha@gmail.com>
2025-11-14 23:43:02 +0700

    initialized design system and folder structures

---
+++ app/components/MatkulCard.tsx
@@ -0,0 +1,7 @@
+export const Card = () => {
+  return (
+    <div className="rounded-xl bg-[#AA14F0] flex flex-col lg:w-[300px] md:w-[200px] w-[100px] items-center justify-center">
+      <h1>matkul</h1>
+    </div>
+  );
+};
